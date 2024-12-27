321e9964e34ea07920abacb7f7a22800f339b1ed
ludofr3 <ludovic.de-chavagnac@epitech.eu>
2024-12-27 17:56:56 +0900

    feat: Project section

---
+++ src/sections/About.jsx
@@ -0,0 +1,87 @@
+import Globe from "react-globe.gl"
+import { useState } from "react"
+import Button from "../components/Button"
+
+
+const About = () => {
+  const [hasCopied, setHasCopied] = useState(false);
+  const handleCopy = () => {
+    navigator.clipboard.writeText('ludovicdechavagnac@gmail.com');
+    setHasCopied(true);
+    setTimeout(() => {
+      setHasCopied(false);
+    }, 2000)
+  }
+  return (
+    <section className="c-space my-20" id="about">
+      <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img src="/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+            <div>
+              <p className="grid-headtext">Hi, I'm Ludovic</p>
+              <p className="grid-subtext">With 4 years of experience, I search a stage for devloppe my skills.</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img src="/assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
+            <div>
+              <p className="grid-headtext">Tech Stack</p>
+              <p className="grid-subtext">I specialize in C, C++, Rust and Python.</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 xl:row-span-4">
+          <div className="grid-container">
+            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+              <Globe
+                height={326}
+                width={326}
+                backgroundColor="rgba(0, 0, 0, 0)"
+                backgroundImageOpacity={0.5}
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+              />
+            </div>
+            <div>
+              <p className="grid-headtext">
+                I work remotly accross most timezones.
+              </p>
+              <p className="grid-subtext">
+                I'm based in France, with remote work avaible.
+              </p>
+              <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img src="/assets/grid3.png" alt="grid-3" className="w-full sm:h-[276px] h-fit object-contain" />
+            <div>
+              <p className="grid-headtext">My Passion for Coding</p>
+              <p className="grid-subtext">I love solving problems and buildings things throught code.</p>
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img src="/assets/grid4.png" alt="grid-4" className="w-full sm:h-[126px] h-fit object-cover sm:object-top" />
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">Contact me</p>
+              <div className="copy-container" onClick={handleCopy}>
+                <img src={hasCopied ? '/assets/tick.svg' : '/assets/copy.svg'} alt="copy" />
+                <p className="lg:text-2xl md:text-xl text-white">ludovicdechavagnac@gmail</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
