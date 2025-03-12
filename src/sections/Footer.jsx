9a877b146f03abc0fe2b8aff15b73d6f1d57a934
ludofr3 <ludovic.de-chavagnac@epitech.eu>
2025-03-12 14:51:55 +0900

    fix: social network in the footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -10,14 +10,18 @@ const Footer = () => {
       </div>
       <div className="flex gap-3">
         <div className="social-icon">
-          <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
+          <a className="w-1/2 h-1/2" href="https://github.com/Ludofr3" target="_blank" rel="noopener noreferrer">
+            <img src="/assets/github.svg" alt="github" />
+          </a>
         </div>
         <div className="social-icon">
-          <img src="/assets/twitter.svg" alt="github" className="w-1/2 h-1/2" />
+          <a className="w-1/2 h-1/2" href="https://www.linkedin.com/in/ludovic-de-chavagnac" target="_blank" rel="noopener noreferrer">
+            <img src="/assets/linkedin.png" alt="linkedin" />
+          </a>
         </div>
-        <div className="social-icon">
+        {/* <div className="social-icon">
           <img src="/assets/instagram.svg" alt="github" className="w-1/2 h-1/2" />
-        </div>
+        </div> */}
       </div>
       <p >© 2024 Ludovic. All rights reserved</p>
     </section>
