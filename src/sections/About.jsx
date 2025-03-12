910f7581f991d572aca6b5e23fcf66d8c3bf62a2
ludofr3 <ludovic.de-chavagnac@epitech.eu>
2025-03-12 14:00:59 +0900

    fix: contente about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -12,30 +12,45 @@ const About = () => {
     }, 2000)
   }
   return (
-    <section className="c-space my-20" id="about">
-      <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+    <section className="c-space mt-20 mb-5" id="about">
+      <div className="grid xl:grid-cols-3 xl:grid-rows-3 md:grid-cols-2 grid-cols-1 gap-5 h-full">
         <div className="col-span-1 xl:row-span-3">
 
           <div className="grid-container">
-            <img src="/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+            <img src="/assets/grid1.png" alt="grid-1" className="w-full sm:h-[205px] h-fit object-contain" />
             <div>
               <p className="grid-headtext">Hi, I'm Ludovic</p>
-              <p className="grid-subtext">With 4 years of experience, I search a stage for devloppe my skills.</p>
+              <p className="grid-subtext">4th year student developer with solid experience in software development. Based in France, I'm passionate about solving complex problems and creating innovative solutions. After four years of intensive learning, I have acquired an in-depth mastery of many languages and technologies, and am ready to take on new challenges in my 5th year and beyond. Flexible and adaptable, I work both in teams and independently.</p>
             </div>
           </div>
         </div>
         <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
-            <img src="/assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
+            <img src="/assets/hello_world.png" alt="grid-2" className="w-full sm:h-[205px] h-fit object-contain" />
             <div>
-              <p className="grid-headtext">Tech Stack</p>
-              <p className="grid-subtext">I specialize in C, C++, Rust and Python.</p>
+              <p className="grid-headtext">My Technical Arsenal</p>
+              {/* <p className="grid-subtext"> */}
+              <ul className="grid-subtext">
+                <li>
+                  <strong>Versioning :</strong> GitHub, GitLab, Bitbucket
+                </li>
+                <li>
+                  <strong>Langages :</strong> Assembleur, Haskell, C, C++, Rust, Python, Bash
+                </li>
+                <li>
+                  <strong>Web & Mobile :</strong> PHP, CSS, SCSS, HTML, SQL, React Native, Three.js, Angular
+                </li>
+                <li>
+                  <strong>Systèmes & DevOps :</strong> Linux, Docker, Kubernetes, Jenkins, Ansible
+                </li>
+              </ul>
+              {/* </p> */}
             </div>
           </div>
         </div>
         <div className="col-span-1 xl:row-span-4">
           <div className="grid-container">
-            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+            <div className="rounded-3xl w-full sm:h-[226px] h-fit flex justify-center items-center">
               <Globe
                 height={326}
                 width={326}
@@ -60,16 +75,16 @@ const About = () => {
         </div>
         <div className="xl:col-span-2 xl:row-span-3">
           <div className="grid-container">
-            <img src="/assets/grid3.png" alt="grid-3" className="w-full sm:h-[276px] h-fit object-contain" />
+            {/* <img src="/assets/grid3.png" alt="grid-3" className="w-full sm:h-[276px] h-fit object-contain" /> */}
             <div>
-              <p className="grid-headtext">My Passion for Coding</p>
-              <p className="grid-subtext">I love solving problems and buildings things throught code.</p>
+              <p className="grid-headtext">My Tech Vision</p>
+              <p className="grid-subtext">For me, development is an opportunity to constantly learn and grow. I love exploring new languages and technologies, understanding how they work, and using them to create effective solutions. My aim is to continue to evolve in a constantly changing field, while bringing my rigor and creativity to every project.</p>
             </div>
           </div>
         </div>
         <div className="xl:col-span-1 xl:row-span-2">
           <div className="grid-container">
-            <img src="/assets/grid4.png" alt="grid-4" className="w-full sm:h-[126px] h-fit object-cover sm:object-top" />
+            {/* <img src="/assets/grid4.png" alt="grid-4" className="w-full sm:h-[126px] h-fit object-cover sm:object-top" /> */}
             <div className="space-y-2">
               <p className="grid-subtext text-center">Contact me</p>
               <div className="copy-container" onClick={handleCopy}>
