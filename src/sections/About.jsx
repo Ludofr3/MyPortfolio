84eb205308dd102aa28bc1d5efc1f3c0906c7775
ludofr3 <ludovic.de-chavagnac@epitech.eu>
2025-03-18 15:51:17 +0900

    fix: email address

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -89,7 +89,7 @@ const About = () => {
               <p className="grid-subtext text-center">Contact me</p>
               <div className="copy-container" onClick={handleCopy}>
                 <img src={hasCopied ? '/assets/tick.svg' : '/assets/copy.svg'} alt="copy" />
-                <p className="lg:text-2xl md:text-xl text-white">ludovicdechavagnac@gmail</p>
+                <p className="lg:text-2xl md:text-xl text-white">ludovicdechavagnac@gmail.com</p>
               </div>
             </div>
           </div>
